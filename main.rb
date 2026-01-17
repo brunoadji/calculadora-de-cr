308295dbdebe4f97e731342e3bc1778acaea9652
OverMan-647 <brunoadji@id.uff.br>
2026-01-17 14:47:35 -0300

    Prettier output

--- main.rb
+++ main.rb
@@ -12,10 +12,11 @@ faculdade = leitor.le_arquivo(arquivo)
 for curso in faculdade.cursos
     carga_horaria_curso, total_nota_curso = curso.calcula_hora_e_notas
     crCurso = carga_horaria_curso == 0 ? 0 : total_nota_curso / carga_horaria_curso
-    puts "Curso: #{curso.codigo_curso} - CR: #{crCurso.round(2)}"
+    puts "- Curso: #{curso.codigo_curso} - CR: #{crCurso.round(2)}"
     for aluno in curso.alunos
         carga_horaria, totalNota = aluno.calcula_hora_e_notas
         crAluno = carga_horaria == 0 ? 0 : totalNota / carga_horaria
-        puts "  Aluno: #{aluno.matricula} - CR: #{crAluno.round(2)}"
+        puts "-- Aluno: #{aluno.matricula} - CR: #{crAluno.round(2)}"
     end
+    puts ""
 end
