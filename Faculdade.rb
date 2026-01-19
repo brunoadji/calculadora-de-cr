2f4ef0c67ed4d22e60f86d3372423a3cb25d9a45
OverMan-647 <brunoadji@id.uff.br>
2026-01-19 17:56:46 -0300

    Removing ternary operator since never will divide by 0

--- Faculdade.rb
+++ Faculdade.rb
@@ -9,11 +9,11 @@ class Faculdade
   def imprime_cr()
     for curso in @cursos
         carga_horaria_curso, total_nota_curso = curso.calcula_hora_e_notas
-        crCurso = carga_horaria_curso == 0 ? 0 : total_nota_curso / carga_horaria_curso
+        crCurso = total_nota_curso / carga_horaria_curso
         puts "- Curso: #{curso.codigo_curso} - CR: #{crCurso.round(2)}"
         for aluno in curso.alunos
             carga_horaria, totalNota = aluno.calcula_hora_e_notas
-            crAluno = carga_horaria == 0 ? 0 : totalNota / carga_horaria
+            crAluno = totalNota / carga_horaria
             puts "-- Aluno: #{aluno.matricula} - CR: #{crAluno.round(2)}"
         end
         puts ""
